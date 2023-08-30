74d4fa773b9fb4892ddd07b680cc6c53d6a72d58
fco15 <100013789+fco15@users.noreply.github.com>
2023-08-30 08:20:26 -0600

    Update PetStoreSystema5q4.java

--- PetStoreSystema5q4.java
+++ PetStoreSystema5q4.java
@@ -1,24 +1,3 @@
-/*
-  CMPT 270 Course Material
-  Copyright (c) 2022
-  All rights reserved.
-
-  This document contains resources for homework assigned to students of
-  of CMPT 270 and shall not be distributed without permission.  Posting this
-  file to a public or private website, or providing this file to any person
-  not registered in CMPT 270 constitutes Academic Misconduct according to
-  to the University of Saskatchewan Policy on Academic Misconduct.
-
-  Synopsis: Starter file for Assignment 5
- */
-
-/**
- * Name: Feranmi Oluwasikun
- * NSID: CSB904
- * Student Number: 11315767
- * Class: CMPT 270 03
- */
-
 import java.util.TreeMap;
 import java.util.Collection;
 import java.util.*;
